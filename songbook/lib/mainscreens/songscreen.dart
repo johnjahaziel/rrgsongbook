d1bebfc1f80b585e6f9b984279830eb8ff67ca57
johnjahaziel <johnjahaziel00@gmail.com>
2024-11-06 22:07:32 +0530

    Initial commit

--- songbook/lib/mainscreens/songscreen.dart
+++ songbook/lib/mainscreens/songscreen.dart
@@ -50,107 +50,109 @@ class _SongscreenState extends State<Songscreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Stack(
-            children: [
-              const SizedBox(
-                height: 200,
-                child: Image(
-                  image: AssetImage('images/Ellipse2.png'),
-                  fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(
+              children: [
+                const SizedBox(
+                  height: 200,
+                  child: Image(
+                    image: AssetImage('images/Ellipse2.png'),
+                    fit: BoxFit.cover,
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 35, left: 20, right: 20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    ShaderMask(
-                      shaderCallback: (Rect bounds) {
-                        return gradient.createShader(bounds);
-                      },
-                      child: Text(
-                        formatTitle(widget.songTitle),
-                        style: const TextStyle(
-                          fontFamily: 'BalsamiqSans',
-                          fontSize: 28,
-                          color: Colors.white,
-                        ),
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.pop(context);
-                      },
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(50),
-                          color: Colors.white,
-                        ),
-                        height: 40,
-                        width: 40,
-                        child: const Icon(
-                          Icons.arrow_back_rounded,
-                          size: 25,
-                          weight: 2,
-                          color: kblack,
+                Padding(
+                  padding: const EdgeInsets.only(top: 35, left: 20, right: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ShaderMask(
+                        shaderCallback: (Rect bounds) {
+                          return gradient.createShader(bounds);
+                        },
+                        child: Text(
+                          formatTitle(widget.songTitle),
+                          style: const TextStyle(
+                            fontFamily: 'BalsamiqSans',
+                            fontSize: 28,
+                            color: Colors.white,
+                          ),
+                          overflow: TextOverflow.ellipsis,
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 128, left: 125),
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    color: kyellow,
-                    boxShadow: const [
-                      BoxShadow(
-                        offset: Offset(0, 2),
-                        color: Colors.grey,
-                        blurRadius: 2,
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                            color: Colors.white,
+                          ),
+                          height: 40,
+                          width: 40,
+                          child: const Icon(
+                            Icons.arrow_back_rounded,
+                            size: 25,
+                            weight: 2,
+                            color: kblack,
+                          ),
+                        ),
                       ),
                     ],
                   ),
-                  height: 38,
-                  width: 160,
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 3),
-                    child: Center(
-                      child: Text(
-                        'Album: ${widget.albumName}',
-                        style: const TextStyle(
-                          fontFamily: 'BalsamiqSans',
-                          fontSize: 18,
-                          color: kblack,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 128, left: 125),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: kyellow,
+                      boxShadow: const [
+                        BoxShadow(
+                          offset: Offset(0, 2),
+                          color: Colors.grey,
+                          blurRadius: 2,
+                        ),
+                      ],
+                    ),
+                    height: 38,
+                    width: 160,
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 3),
+                      child: Center(
+                        child: Text(
+                          'Album: ${widget.albumName}',
+                          style: const TextStyle(
+                            fontFamily: 'BalsamiqSans',
+                            fontSize: 18,
+                            color: kblack,
+                          ),
                         ),
                       ),
                     ),
                   ),
                 ),
-              ),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20),
-            child: Text(
-              widget.lyrics.join("\n"),
-              style: TextStyle(
-                fontFamily: 'BalsamiqSans',
-                fontSize: _fontSize,
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Text(
+                widget.lyrics.join("\n"),
+                style: TextStyle(
+                  fontFamily: 'BalsamiqSans',
+                  fontSize: _fontSize,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- songbook/lib/sidemenuscreens/albumscreen.dart
+++ songbook/lib/sidemenuscreens/albumscreen.dart
@@ -15,6 +15,10 @@ class _AlbumscreenState extends State<Albumscreen> {
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double padding = screenWidth * 0.1;
+    double spacing = screenWidth * 0.08;
+
     return Scaffold(
       appBar: AppBar(
         title: ShaderMask(
@@ -35,224 +39,55 @@ class _AlbumscreenState extends State<Albumscreen> {
       ),
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.only(top: 20, left: 35, right: 35),
+          padding: EdgeInsets.only(top: 20, left: padding, right: padding),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  AlbumWidget(
-                    title: 'Volume - 1',
-                    imagePath: 'images/vol2.jpg',
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const AlbumDetailsScreen(
-                            albumTitle: 'Volume - 1',
+              ...List.generate(6, (rowIndex) {
+                int startIndex = rowIndex * 2 + 1;
+                return Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
+                          child: AlbumWidget(
+                            title: 'Volume - $startIndex',
+                            imagePath: 'images/vol$startIndex.jpg',
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => AlbumDetailsScreen(
+                                    albumTitle: 'Volume - $startIndex',
+                                  ),
+                                ),
+                              );
+                            },
                           ),
                         ),
-                      );
-                    },
-                  ),
-                  AlbumWidget(
-                    title: 'Volume - 2',
-                    imagePath: 'images/vol2.jpg',
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const AlbumDetailsScreen(
-                            albumTitle: 'Volume - 2',
+                        SizedBox(width: spacing),
+                        Expanded(
+                          child: AlbumWidget(
+                            title: 'Volume - ${startIndex + 1}',
+                            imagePath: 'images/vol${startIndex + 1}.jpg',
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => AlbumDetailsScreen(
+                                    albumTitle: 'Volume - ${startIndex + 1}',
+                                  ),
+                                ),
+                              );
+                            },
                           ),
                         ),
-                      );
-                    },
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  AlbumWidget(
-                    title: 'Volume - 3',
-                    imagePath: 'images/vol3.jpg',
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const AlbumDetailsScreen(
-                            albumTitle: 'Volume - 3',
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                  AlbumWidget(
-                    title: 'Volume - 4',
-                    imagePath: 'images/vol4.jpg',
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const AlbumDetailsScreen(
-                            albumTitle: 'Volume - 4',
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  AlbumWidget(
-                    title: 'Volume - 5',
-                    imagePath: 'images/vol5.jpg',
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const AlbumDetailsScreen(
-                            albumTitle: 'Volume - 5',
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                  AlbumWidget(
-                    title: 'Volume - 6',
-                    imagePath: 'images/vol6.jpg',
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const AlbumDetailsScreen(
-                            albumTitle: 'Volume - 6',
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  AlbumWidget(
-                    title: 'Volume - 7',
-                    imagePath: 'images/vol7.jpg',
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const AlbumDetailsScreen(
-                            albumTitle: 'Volume - 7',
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                  AlbumWidget(
-                    title: 'Volume - 8',
-                    imagePath: 'images/vol8.jpg',
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const AlbumDetailsScreen(
-                            albumTitle: 'Volume - 8',
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  AlbumWidget(
-                    title: 'Volume - 9',
-                    imagePath: 'images/vol9.jpg',
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const AlbumDetailsScreen(
-                            albumTitle: 'Volume - 9',
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                  AlbumWidget(
-                    title: 'Volume - 10',
-                    imagePath: 'images/vol10.jpg',
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const AlbumDetailsScreen(
-                            albumTitle: 'Volume - 10',
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  AlbumWidget(
-                    title: 'Volume - 11',
-                    imagePath: 'images/vol11.jpg',
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const AlbumDetailsScreen(
-                            albumTitle: 'Volume - 11',
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                  AlbumWidget(
-                    title: 'Volume - 12',
-                    imagePath: 'images/vol12.jpg',
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const AlbumDetailsScreen(
-                            albumTitle: 'Volume - 12',
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                ],
-              ),
+                      ],
+                    ),
+                    SizedBox(height: spacing),
+                  ],
+                );
+              }),
             ],
           ),
         ),
